f26e0e6f03b5a77ff4ad92791768ec1a859634c7
RajeshVattam45 <>
2026-02-06 14:08:23 +0530

    Updated the SDC component module

--- web/themes/custom/tech_comp/components/accordion-item/accordion-item.js
+++ web/themes/custom/tech_comp/components/accordion-item/accordion-item.js
@@ -1,16 +1,32 @@
-(function () {
-  document.addEventListener('click', function (e) {
-    const header = e.target.closest('.accordion-item__header');
-    if (!header) return;
+(function (Drupal, once) {
+  // Accordion behavior (AJAX-safe).
+  Drupal.behaviors.accordionItem = {
 
-    const item = header.closest('.accordion-item');
+    // Attach click handler once per header.
+    attach(context) {
 
-    document.querySelectorAll('.accordion-item').forEach(el => {
-      if (el !== item) {
-        el.classList.remove('is-open');
-      }
-    });
+      // Use once() to ensure the click handler is attached.
+      // only one time per accordion header, This prevents duplicate event listeners.
+      once('accordion-item', '.accordion-item__header', context)
+        .forEach((header) => {
 
-    item.classList.toggle('is-open');
-  });
-})();
+          // Attach click event to the accordion header button
+          header.addEventListener('click', function () {
+            const item = header.closest('.accordion-item');
+
+            // Close all other accordion items.
+            // This ensures only one accordion is open at a time.
+            document.querySelectorAll('.accordion-item').forEach((el) => {
+              if (el !== item) {
+                el.classList.remove('is-open');
+              }
+            });
+
+            // Toggle the current accordion item's open state.
+            item.classList.toggle('is-open');
+          });
+
+        });
+    }
+  };
+})(Drupal, once);
