fb6362670378e3fb6cec75cdd69cf6d6b859181b
RajeshVattam45 <>
2026-02-09 10:33:34 +0530

    Updated DB and removed the group twig template and updated configurations.

---
+++ web/sites/default/files/php/twig/69896775559c5_accordion-item.twig_8JQoekoJqz5QaTyDbCaFFoaSa/zsEHplbzROWrsZegmHje0e--SibxrrCndujS8spI9gQ.php
@@ -0,0 +1,176 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* @tech_comp/components/accordion-item/accordion-item.twig */
+class __TwigTemplate_ddf1efbbd590449eb27bd35cb37d5b68 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 1
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("tech_comp/accordion"), "html", null, true);
+        yield "
+";
+        // line 2
+        $context["accordion_id"] = ("accordion-item-" . Twig\Extension\CoreExtension::random($this->env->getCharset()));
+        // line 3
+        yield "
+<div class=\"accordion-item\" data-accordion-item>
+  <button
+    class=\"accordion-item__header\"
+    type=\"button\"
+    aria-expanded=\"false\"
+    aria-controls=\"";
+        // line 9
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["accordion_id"] ?? null), "html", null, true);
+        yield "\"
+    data-accordion-trigger
+  >
+    <span class=\"accordion-item__title\">";
+        // line 12
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["title"] ?? null), "html", null, true);
+        yield "</span>
+    <span class=\"accordion-item__icon\" aria-hidden=\"true\"></span>
+  </button>
+
+  <div
+    id=\"";
+        // line 17
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["accordion_id"] ?? null), "html", null, true);
+        yield "\"
+    class=\"accordion-item__content\"
+    role=\"region\"
+    aria-hidden=\"true\"
+    data-accordion-content
+  >
+    <div class=\"accordion-item__description\">
+      ";
+        // line 24
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["description"] ?? null), "html", null, true);
+        yield "
+    </div>
+  </div>
+</div>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["title", "description"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "@tech_comp/components/accordion-item/accordion-item.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  82 => 24,  72 => 17,  64 => 12,  58 => 9,  50 => 3,  48 => 2,  44 => 1,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("{{ attach_library('tech_comp/accordion') }}
+{% set accordion_id = 'accordion-item-' ~ random() %}
+
+<div class=\"accordion-item\" data-accordion-item>
+  <button
+    class=\"accordion-item__header\"
+    type=\"button\"
+    aria-expanded=\"false\"
+    aria-controls=\"{{ accordion_id }}\"
+    data-accordion-trigger
+  >
+    <span class=\"accordion-item__title\">{{ title }}</span>
+    <span class=\"accordion-item__icon\" aria-hidden=\"true\"></span>
+  </button>
+
+  <div
+    id=\"{{ accordion_id }}\"
+    class=\"accordion-item__content\"
+    role=\"region\"
+    aria-hidden=\"true\"
+    data-accordion-content
+  >
+    <div class=\"accordion-item__description\">
+      {{ description }}
+    </div>
+  </div>
+</div>
+", "@tech_comp/components/accordion-item/accordion-item.twig", "C:\\xampp\\htdocs\\drupal_sdc\\web\\themes\\custom\\tech_comp\\components\\accordion-item\\accordion-item.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["set" => 2];
+        static $filters = ["escape" => 1];
+        static $functions = ["attach_library" => 1, "random" => 2];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                ['escape'],
+                ['attach_library', 'random'],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
--- web/themes/custom/tech_comp/components/accordion-item/accordion-item.js
+++ web/themes/custom/tech_comp/components/accordion-item/accordion-item.js
@@ -1,30 +1,40 @@
 (function (Drupal, once) {
   Drupal.behaviors.accordionItem = {
     attach(context) {
-      once('accordion-item', '[data-accordion-trigger]', context)
-        .forEach((trigger) => {
 
-          trigger.addEventListener('click', () => {
-            const item = trigger.closest('[data-accordion-item]');
+      once('accordion-group', '[data-accordion-group]', context)
+        .forEach((group) => {
+
+          const items = group.querySelectorAll('[data-accordion-item]');
+
+          items.forEach((item) => {
+            const trigger = item.querySelector('[data-accordion-trigger]');
             const content = item.querySelector('[data-accordion-content]');
-            const isOpen = item.classList.contains('is-open');
 
-            // Close all other accordion items
-            document.querySelectorAll('[data-accordion-item]').forEach((el) => {
-              const btn = el.querySelector('[data-accordion-trigger]');
-              const cnt = el.querySelector('[data-accordion-content]');
-
-              el.classList.remove('is-open');
-              btn.setAttribute('aria-expanded', 'false');
-              cnt.setAttribute('aria-hidden', 'true');
-            });
-
-            // Toggle current accordion
-            if (!isOpen) {
-              item.classList.add('is-open');
-              trigger.setAttribute('aria-expanded', 'true');
-              content.setAttribute('aria-hidden', 'false');
+            if (!trigger || !content) {
+              return;
             }
+
+            trigger.addEventListener('click', () => {
+              const isOpen = item.classList.contains('is-open');
+
+              // Close only siblings inside THIS block
+              items.forEach((el) => {
+                const btn = el.querySelector('[data-accordion-trigger]');
+                const cnt = el.querySelector('[data-accordion-content]');
+
+                el.classList.remove('is-open');
+                btn?.setAttribute('aria-expanded', 'false');
+                cnt?.setAttribute('aria-hidden', 'true');
+              });
+
+              // Open current
+              if (!isOpen) {
+                item.classList.add('is-open');
+                trigger.setAttribute('aria-expanded', 'true');
+                content.setAttribute('aria-hidden', 'false');
+              }
+            });
           });
 
         });
